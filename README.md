279e8651909caa110f96a92b3bc5921730f94e7f
NateScarlet <NateScarlet@Gmail.com>
2020-06-04 23:19:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,72 @@
+# gotmpl
+
+[![build status](https://github.com/NateScarlet/gotmpl/workflows/go/badge.svg)](https://github.com/NateScarlet/gotmpl/actions)
+
+Render [golang template](https://pkg.go.dev/text/template/) with command line.
+
+[templates](./templates) folder has some example templates.
+
+## Usage
+
+```shell
+go get github.com/NateScarlet/gotmpl/cmd/gotmpl
+```
+
+Render template to stdout
+
+```shell
+gotmpl ./template.gotmpl
+```
+
+Render template with data
+
+```shell
+gotmpl Key=Value ./template.gotmpl
+```
+
+Render template that file name has `=` in it.
+
+```shell
+gotmpl =./template=.gotmpl
+```
+
+Render to a file.
+
+```shell
+gotmpl -o ./output.txt ./template.gotmpl
+```
+
+Render multiple template to stdout.
+first template executed, others can be used with `template` action.
+
+```shell
+gotmpl Key=Value ./template.gotmpl ./template2.gotmpl
+```
+
+Read data from stdin
+
+```shell
+echo '{"Key": "Value"}' | gotmpl ./template.gotmpl
+```
+
+## Template
+
+### Functions
+
+- [sprig](https://masterminds.github.io/sprig/) functions
+- `cwd`() current working directory.
+- `args`() return command line argument list.
+- `templateFiles`() return template file list.
+- `__file__`() value of `-o` option, empty string if not specified.
+- `absPath`(string) convert path to absolute.
+- `lowerFirst`(string) lower first rune of string
+- `upperFirst`(string) upper first rune of string
+
+### Data
+
+The "." object is a `map[string]interface{}`, so you can use `set` function to modify it.
+
+There is some default data for convenient:
+
+- `Name`: `index (__file__ | absPath | base | split ".") 0` if `__file__` not empty
+- `Package` `__file__ | absPath | dir | base` if `__file__` not empty
---
+++ cmd/gotmpl/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/Masterminds/sprig"
+	"github.com/huandu/xstrings"
+)
+
+func stdinData() map[string]interface{} {
+	// https://stackoverflow.com/questions/22744443/
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) != 0 {
+		return nil
+	}
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data = map[string]interface{}{}
+	err = json.Unmarshal(stdin, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
+
+var output string
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprint(flag.CommandLine.Output(),
+			`Usage: gotmpl [-o output] [key=value] template [template ...]
+You can also pass a json dict to stdin as data.
+
+Version 0.1.0
+https://github.com/NateScarlet/gotmpl
+`)
+	}
+	flag.StringVar(&output, "o", "", "output file path")
+	flag.Parse()
+
+	data := map[string]interface{}{} // sprig dict functions require map[string]interface{}
+	files := []string{}
+
+	if output != "" {
+		p, err := filepath.Abs(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data["Name"] = strings.SplitN(filepath.Base(p), ".", 2)[0]
+		data["Package"] = filepath.Base(filepath.Dir(p))
+	}
+
+	for k, v := range stdinData() {
+		data[k] = v
+	}
+
+	for _, i := range flag.Args() {
+		if strings.Contains(i, "=") {
+			kv := strings.SplitN(i, "=", 2)
+			if kv[0] == "" {
+				files = append(files, kv[1])
+				continue
+			}
+			data[kv[0]] = kv[1]
+		} else {
+			files = append(files, i)
+		}
+	}
+
+	if len(files) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	funcs := sprig.TxtFuncMap()
+	funcs["cwd"] = os.Getwd
+	funcs["args"] = func() []string { return os.Args }
+	funcs["templateFiles"] = func() []string { return files }
+	funcs["__file__"] = func() string { return output }
+	funcs["absPath"] = filepath.Abs
+	funcs["upperFirst"] = xstrings.FirstRuneToUpper
+	funcs["lowerFirst"] = xstrings.FirstRuneToLower
+
+	tmpl, err := template.
+		New(filepath.Base(files[0])).
+		Funcs(funcs).
+		ParseFiles(files...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b := new(bytes.Buffer)
+	err = tmpl.Execute(b, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if output != "" {
+		var err = ioutil.WriteFile(output, b.Bytes(), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Print(b.String())
+	}
+}
